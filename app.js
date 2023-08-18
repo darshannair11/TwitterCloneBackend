ac10db01e23f33d763d884016ae2f3f66ddf114a
dsasidharannair <105741564+dsasidharannair@users.noreply.github.com>
2023-08-18 09:18:59 +0400

    backtonormal

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express=require("express");
 const bodyParser=require("body-parser");
 const mongoose=require("mongoose");
-const session = require('cookie-session');
+const session = require('express-session');
 const passport=require("passport");
 const passportLocalMongoose=require("passport-local-mongoose");
 const findOrCreate=require("mongoose-findorcreate");
